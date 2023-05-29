af1c0085ff06aca57ecb335348a470503a35cdae
MridulSheikh <prince9nazir@gmail.com>
2023-05-29 21:14:22 +0600

    add shop page

---
+++ mockdata/BRAND_MOCK_DATA.ts
@@ -0,0 +1,12 @@
+const brand_mock_data = [
+    {
+        name : "longines",
+        img : "/images/watch-logo-explainer-longines-1-1625096293.jpg"
+    },
+    {
+        name : "bregutel",
+        img : "/images/watch-logo-explainer-breguet-1625095803.jpg"
+    }
+]
+
+export default brand_mock_data;
